923163b2c6b519bf9221cf25f69e0ac4e4cd5d9b
sanu <sanooj171@gmail.com>
2023-10-15 19:56:56 +0530

    Updated file

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -4,37 +4,33 @@ const todoList = require("../todo");
 const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todolist Test Suite", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     // Clear the todo list before each test
     all.length = 0;
-
-    // Add a test todo item with a future due date
-    const futureDueDate = new Date();
-    futureDueDate.setDate(futureDueDate.getDate() + 1);
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: futureDueDate.toISOString(),
-    });
   });
 
-  test("Should add new todo", () => {
+  test("Creating a new todo", () => {
     const todoItemsCount = all.length;
     add({
-      title: "Test todo",
+      title: "New todo",
       completed: false,
       dueDate: new Date().toISOString(),
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
 
-  test("Should mark a todo as complete", () => {
+  test("Marking a todo as completed", () => {
+    add({
+      title: "Todo to be completed",
+      completed: false,
+      dueDate: new Date().toISOString(),
+    });
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
 
-  test("Checks retrieval of Overdue items", () => {
+  test("Retrieval of overdue items", () => {
     const pastDueDate = new Date();
     pastDueDate.setDate(pastDueDate.getDate() - 1);
     add({
@@ -46,7 +42,7 @@ describe("Todolist Test Suite", () => {
     expect(overdue().length).toBe(1);
   });
 
-  test("Checks retrieval of Duetoday items", () => {
+  test("Retrieval of due today items", () => {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
     add({
@@ -58,7 +54,7 @@ describe("Todolist Test Suite", () => {
     expect(dueToday().length).toBe(0);
   });
 
-  test("Checks retrieval of Duelater items", () => {
+  test("Retrieval of due later items", () => {
     const futureDueDate = new Date();
     futureDueDate.setDate(futureDueDate.getDate() + 2);
     add({
@@ -67,6 +63,6 @@ describe("Todolist Test Suite", () => {
       dueDate: futureDueDate.toISOString(),
     });
 
-    expect(dueLater().length).toBe(3);
+    expect(dueLater().length).toBe(1);
   });
 });
